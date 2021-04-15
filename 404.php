f1984409aa12ca1a1275bf45736bc7f8797f159f
Melvin Debot <melvin@exceenis.com>
2021-04-15 16:11:29 +0200

    back: Template hierarchy

---
+++ 404.php
@@ -0,0 +1,6 @@
+<?php get_header() ?>
+
+  <h1>Page Introuvable</h1>
+  <p>Cette page n'exsite pas</p>
+
+<?php get_footer() ?>
---
+++ front-page.php
@@ -0,0 +1,10 @@
+<!-- PAGE HOME -->
+<?php get_header() ?>
+
+<?php while(have_posts()) : the_post() ?>
+  <h1><?php the_title() ?></h1>
+  <?php the_content() ?>
+  <a href="<?= get_post_type_archive_link('post') ?>" >Voir les dernieres actualités</a>
+<?php endwhile; ?>
+
+<?php get_footer() ?>
